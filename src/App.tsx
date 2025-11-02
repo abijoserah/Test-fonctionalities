cd32ff279c0aadc3f67a65064376c8051765fced
abijoserah <serahabijo94@gmail.com>
2025-11-02 20:55:35 +0100

    search songs, remove songs

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import './App.css'
 import { songs as initialSongs } from "../data"
 import { useState } from 'react'
 
+// {"id": 1, "title":"Ed Sheeran"}
+// {"id": 2, "title":"Justin Bieber"}
+// {"id": 3, "title":"Jason Derulo"}
+// {"id": 4, "title":"Bruno Mars"}
+// songs.filter((song) => song.id !== 3)
+// 
 
 function App() {
   const [search, setSearch] = useState("")
-  const [songs, setSongs] = useState(initialSongs)
+  const [songs, setSongs] = useState(initialSongs.slice(0, 6))
   const filteredSongs = songs.filter((song) => (song.title.toLowerCase().includes(search.toLowerCase())))
 
   function handleRemoveSong(songId: string) {
-    setSongs(songs.filter((song) => (songId !== song.id)))
+    const keptSongs = songs.filter((song) => (songId !== song.id))
+    setSongs(keptSongs)
   }
 
 
@@ -28,7 +35,10 @@ function App() {
         {search ? <button onClick={() => (setSearch(""))}>Reset</button> : null}
       </div>
       <ul className='text-start flex flex-col gap-1 text-xs'>
-        {filteredSongs.map((song) => (<li className='rounded-sm bg-slate-100 px-1 py-2'><span>{song.title}</span>
+        {filteredSongs.map((song) => (<li className='rounded-sm bg-slate-100 px-1 py-2'>
+
+          <img src={song.cover} className='w-full h-auto max-w-16 rounded-sm' />
+          <span>{song.title}</span>
           <div className='items-end'>
 
             <button onClick={() => handleRemoveSong(song.id)}>X</button>
